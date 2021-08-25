d0842468229791200ec0643e02ae110d79a20312
Arpad Szocs <arpad.szocs@msg.group>
2021-08-25 17:29:36 +0300

    Fixes

--- src/main/java/app/entity/User.java
+++ src/main/java/app/entity/User.java
@@ -1,10 +1,17 @@
 package app.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * TODO Class description.
@@ -12,9 +19,15 @@ import javax.persistence.Table;
  * @author Szocs, Arpad (arpad.szocs@msg.group)
  * @since 8/23/2021
  */
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "USER")
-public class User{
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class User implements Serializable {
 
     @Id
     @GeneratedValue
--- src/main/java/app/main/Main.java
+++ src/main/java/app/main/Main.java
@@ -2,10 +2,18 @@ package app.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "app.repository")
+@ComponentScan(basePackages = {"app.service",
+                               "app.controller"})
+@EntityScan(basePackages = "app.entity")
 public class Main {
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
